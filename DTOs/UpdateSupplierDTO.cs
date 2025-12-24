d90fb5d8d9b68e3a0370950393a092a221979ad1
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-24 12:15:24 +0200

    Add UpdateSupplierDTO

---
+++ DTOs/UpdateSupplierDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FactoriesGateSystem.DTOs
+{
+    public class UpdateSupplierDTO
+    {
+        public required string Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Phone { get; set; }
+
+        public int CurrentBalance { get; set; }
+    }
+}
