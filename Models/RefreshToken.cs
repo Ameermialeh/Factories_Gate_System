9759cf9db1afda685d087d1446905c9a6af1b7ec
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-26 14:55:27 +0200

    Add RefreshToken.cs

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FactoriesGateSystem.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public bool IsRevoked { get; set; }
+
+        public int ManagerId { get; set; }
+
+        [ForeignKey(nameof(ManagerId))]
+        public Manager Manager { get; set; }
+    }
+}
