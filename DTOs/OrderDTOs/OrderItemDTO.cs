aaa023724665dd1ea0305076527aa1a897c59968
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-27 12:00:58 +0200

    Rename class OrderProductDTO to OrderItemDTO

---
+++ DTOs/OrderDTOs/OrderItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoriesGateSystem.DTOs.OrderDTOs
+{
+    public class OrderItemDTO
+    {
+        public int ProductID { get; set; }
+
+        public int ProductQuantity { get; set; }
+    }
+}
