7d5c838ed9bd0f5210b99c676abb2be1e8a42c61
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-28 13:01:11 +0200

    Add ManagerDTO

---
+++ DTOs/ManagerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.DTOs
+{
+    public class ManagerDTO
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
