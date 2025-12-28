1b301c06cc5415eeb3ba2b4b0ae9b3782fac92a5
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-28 13:43:20 +0200

    Add AdminDTO

---
+++ DTOs/Admin/RegisterAdminDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoriesGateSystem.DTOs.Admin
+{
+    public class RegisterAdminDTO
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
