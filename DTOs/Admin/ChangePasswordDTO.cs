3cc71f8df8dae892d67608c62bdf7900a32911fc
Ameer Mialeh <ameerinad91@gmail.com>
2025-12-28 14:57:43 +0200

    Add ChangePasswordDTO

---
+++ DTOs/Admin/ChangePasswordDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.DTOs.Admin
+{
+    public class ChangePasswordDTO
+    {
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required string CurrentPassword { get; set; }
+        [Required, MinLength(8)]
+        public required string NewPassword { get; set; }
+    }
+}
