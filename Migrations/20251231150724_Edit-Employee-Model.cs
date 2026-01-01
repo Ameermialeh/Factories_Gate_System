4f1d2a2ad92372e2b94b30934e5e3d5d24f593e2
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-01 14:24:59 +0200

    Edit Employee Model

---
+++ Migrations/20251231150724_Edit-Employee-Model.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactoriesGateSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditEmployeeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FactoryId",
+                table: "vacations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FactoryId",
+                table: "employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vacations_FactoryId",
+                table: "vacations",
+                column: "FactoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_vacations_employees_FactoryId",
+                table: "vacations",
+                column: "FactoryId",
+                principalTable: "employees",
+                principalColumn: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_vacations_employees_FactoryId",
+                table: "vacations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_vacations_FactoryId",
+                table: "vacations");
+
+            migrationBuilder.DropColumn(
+                name: "FactoryId",
+                table: "vacations");
+
+            migrationBuilder.DropColumn(
+                name: "FactoryId",
+                table: "employees");
+        }
+    }
+}
