5beac2b03d4adaf8ce24e01bb5d901d9bdee8c22
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-02 15:25:23 +0200

    Add UpdateVacationDTO

---
+++ DTOs/VacationDTOs/UpdateVacationDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace FactoriesGateSystem.DTOs.VacationDTOs
+{
+    public class UpdateVacationDTO
+    {
+        public class UpdateVacationDate
+        {
+            public int VacationId { get; set; }
+            public DateTime FromDate { get; set; }
+            public DateTime ToDate { get; set; }
+        }
+
+        public class  UpdateVacationReasone
+        {
+            public int VacationId { get; set; }
+            public string? VacationReason { get; set; }
+        }
+    }
+}
