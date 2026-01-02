84aa0ae34402af2003623eca36f5b61d587a5a40
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-02 14:51:52 +0200

    Add GetAllVacations GET API

--- Controllers/VacationController.cs
+++ Controllers/VacationController.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FactoriesGateSystem.DTOs;
+using FactoriesGateSystem.DTOs.EmployeeDTOs;
+using FactoriesGateSystem.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FactoriesGateSystem.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "manager")]
     public class VacationController : Controller
     {
-        private readonly VacationController _vacationRepo;
+        private readonly VacationRepo _vacationRepo;
 
-        public VacationController(VacationController vacationRepo)
+        public VacationController(VacationRepo vacationRepo)
         {
             _vacationRepo = vacationRepo;
         }
 
+        [HttpGet]
+        [ProducesResponseType(typeof(VacationDTO), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetAllVacation()
+        {
+            try
+            {
+                var vacations = await _vacationRepo.GetAllVacationAsync();
+
+                if(vacations == null) { return NotFound("Vacations not Found!"); }
+                return Ok(vacations);
+            }catch { return StatusCode(500, "Internal server error"); }
+        }
 
     }
 }
--- Repositories/VacationRepo.cs
+++ Repositories/VacationRepo.cs
@@ -1,4 +1,10 @@
-﻿namespace FactoriesGateSystem.Repositories
+﻿using FactoriesGateSystem.DTOs;
+using FactoriesGateSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace FactoriesGateSystem.Repositories
 {
     public class VacationRepo
     {
@@ -7,6 +13,22 @@
         public VacationRepo (AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        public async Task<List<VacationDTO>?> GetAllVacationAsync(Expression<Func<Vacation, bool>>? filter = null)
+        {
+            IQueryable<Vacation> query = _appDbContext.vacations;
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.Select(v => new VacationDTO
+            {
+                VacationId = v.VacationId,
+                EmployeeId = v.EmployeeId,
+                FromDate = v.FromDate,
+                ToDate = v.ToDate,  
+                VacationReason = v.VacationReason,
+            }).ToListAsync();
         }
     }
 }
