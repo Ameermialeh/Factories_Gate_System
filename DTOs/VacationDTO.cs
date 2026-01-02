555ce89a1fd60deca3a469efcc2f9194088b3237
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-02 14:50:57 +0200

    Add VacationDTO

---
+++ DTOs/VacationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.DTOs
+{
+    public class VacationDTO
+    {
+        public int VacationId { get; set; }
+       
+        public int EmployeeId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+
+        public string? VacationReason { get; set; }
+    }
+}
