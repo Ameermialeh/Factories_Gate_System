2fa201c06c241d0f81605c82a45ad24ef336aaa1
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-03 14:00:51 +0200

    Add UpdateProductDTO

---
+++ DTOs/ProductDTOs/UpdateProductDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FactoriesGateSystem.DTOs.ProductDTOs
+{
+    public class UpdateProductDTO
+    {
+        public int ID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
