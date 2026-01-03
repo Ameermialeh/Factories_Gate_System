465f84bd855a2665ccc460b8e9be99534617efa9
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-03 15:11:03 +0200

    Add AddMaterialDTO

---
+++ DTOs/MaterialDTOs/AddMaterialDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace FactoriesGateSystem.DTOs.MaterialDTOs
+{
+    public class AddMaterialDTO
+    {
+        public class AddNewMaterialDTO
+        {
+            public string? Name { get; set; }
+
+            public string? Unit { get; set; }
+
+            public int SupplierId { get; set; }
+
+            public decimal PricePerUnit { get; set; }
+
+            public int Quantity {  get; set; }
+        }
+
+        public class AddExistingMaterialDTO
+        {
+            public int MaterialId { get; set; }
+
+            public int SupplierId { get; set; }
+
+            public decimal PricePerUnit { get; set; }
+
+            public int Quantity { get; set; }
+        }
+    }
+}
