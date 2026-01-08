5c21bea1c5fb3900902d93486f303940704b896a
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-08 14:57:04 +0200

    Add Expense DTO

---
+++ DTOs/ExpenseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.DTOs
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
