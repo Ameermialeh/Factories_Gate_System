87279900076c4fe83b40844ee359db313da8b4f8
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-08 15:22:12 +0200

    Add UpdateExpenseDTO

---
+++ DTOs/ExpenseDTOs/UpdateExpenseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace FactoriesGateSystem.DTOs.ExpenseDTOs
+{
+    public class UpdateExpenseDTO
+    {
+        public class UpdateExpenseAmount
+        {
+            public int id;
+            public int newAmount;
+        }
+
+        public class UpdateExpenseDescription
+        {
+            public int id;
+            public string? newDescription;
+        }
+    }
+}
