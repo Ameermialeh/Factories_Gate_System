7b2e000a797e85b605d2ccc974c90b68afdab746
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-08 16:03:06 +0200

    Add RangeDateSalaryDTO

---
+++ DTOs/SalaryDTOs/RangeDateSalaryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FactoriesGateSystem.DTOs.SalaryDTOs
+{
+    public class RangeDateSalaryDTO
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
