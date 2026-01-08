fc20ed3306d5d9513bd0316e68147357b6ff2f51
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-08 15:10:48 +0200

    Add AddExponseDTO

---
+++ DTOs/ExpenseDTOs/AddExpenseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoriesGateSystem.DTOs.ExpenseDTOs
+{
+    public class AddExpenseDTO
+    {
+        public string? Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
