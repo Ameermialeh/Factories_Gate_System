512d467bee84d8c27a5ebab762f215ee53106bdd
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-08 16:23:46 +0200

    Add UpdateSalaryDTO

---
+++ DTOs/SalaryDTOs/UpdateSalaryDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace FactoriesGateSystem.DTOs.SalaryDTOs
+{
+    public class UpdateSalaryDTO
+    {
+        public class UpdateSalariesDTO
+        {
+            public int Id { get; set; }
+            public decimal BaseSalary { get; set; }
+
+            public decimal Bonus { get; set; }
+
+            public decimal Deductions { get; set; }
+        }
+
+        public class UpdateDateSalaryDTO
+        {
+            public int Id { get; set; }
+            public DateTime Date { get; set; }
+        }
+    }
+}
--- obj/Debug/net8.0/FactoriesGateSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/FactoriesGateSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8ad673eaaf18d10bcffe450d4f5f72d61cfabc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cede93e80b1ab2052fb569b542db6d740c4f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
