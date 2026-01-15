b0b3ab8657f99b3c6480420ee42f3e9ec8912007
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-15 19:05:01 +0200

    .

--- obj/Debug/net8.0/FactoriesGateSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/FactoriesGateSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cede93e80b1ab2052fb569b542db6d740c4f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181d9315b40fc71ab07035cdcc9e903a93927ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoriesGateSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
