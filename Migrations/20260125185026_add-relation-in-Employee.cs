3de9a610c6c8d242581df376f051389012914729
Ameer Mialeh <ameerinad91@gmail.com>
2026-01-25 20:59:19 +0200

    Update Relation of employee

---
+++ Migrations/20260125185026_add-relation-in-Employee.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactoriesGateSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class addrelationinEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_FactoryId",
+                table: "employees",
+                column: "FactoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_employees_factory_FactoryId",
+                table: "employees",
+                column: "FactoryId",
+                principalTable: "factory",
+                principalColumn: "FactoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_employees_factory_FactoryId",
+                table: "employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_employees_FactoryId",
+                table: "employees");
+        }
+    }
+}
