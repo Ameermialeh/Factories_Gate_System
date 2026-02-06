fa7063acd048ee2f9398f3fcac7bf3980f2f1a26
Ameer Mialeh <ameerinad91@gmail.com>
2026-02-06 18:12:07 +0200

    Add Create Product DTO

---
+++ Models/DTOs/ProductDTOs/CreateProductDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.Models.DTOs.ProductDTOs
+{
+    public class CreateProductDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string NameAr { get; set; } = string.Empty;
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public int Price { get; set; } 
+    }
+}
