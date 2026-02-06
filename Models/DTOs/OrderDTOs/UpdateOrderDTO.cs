0ccc825740243486f13927b46e07d0dd723a2af2
Ameer Mialeh <ameerinad91@gmail.com>
2026-02-06 18:11:44 +0200

    Add Update Order DTO

---
+++ Models/DTOs/OrderDTOs/UpdateOrderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.Models.DTOs.OrderDTOs
+{
+    public class UpdateOrderDTO
+    {
+        public string? Name { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public int? CustomerID { get; set; }
+
+        public ICollection<OrderItemDTO>? Products { get; set; }
+    }
+}
