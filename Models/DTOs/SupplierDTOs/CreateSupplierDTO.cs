49150aad3032c48322750622faef199a3f71a820
Ameer Mialeh <ameerinad91@gmail.com>
2026-02-06 18:12:27 +0200

    Add Create Supplier DTO

---
+++ Models/DTOs/SupplierDTOs/CreateSupplierDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FactoriesGateSystem.Models.DTOs.SupplierDTOs
+{
+    public class CreateSupplierDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
