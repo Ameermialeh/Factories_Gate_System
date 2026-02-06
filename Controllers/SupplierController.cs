54f7cb91e198e9bb5d4197504ebc52eb0b0cc525
Ameer Mialeh <ameerinad91@gmail.com>
2026-02-06 18:19:38 +0200

    Update Supplier Controller APIs

--- Controllers/SupplierController.cs
+++ Controllers/SupplierController.cs
@@ -1,5 +1,6 @@
 ﻿using FactoriesGateSystem.Models.DTOs.SupplierDTOs;
 using FactoriesGateSystem.Repositories;
+using FactoriesGateSystem.Services.ServiceInterfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,12 @@ namespace FactoriesGateSystem.Controllers
     [Authorize(Roles = "manager")]
     public class SupplierController : Controller
     {
-       private readonly SupplierRepo _supplierRepo;
-        public SupplierController(SupplierRepo supplierRepo)
+        private readonly SupplierRepo _supplierRepo;
+        private readonly ISupplierService _supplierService;
+        public SupplierController(SupplierRepo supplierRepo,ISupplierService supplierService)
         {
             _supplierRepo = supplierRepo;
+            _supplierService = supplierService;
         }
 
         [HttpGet]
@@ -22,17 +25,13 @@ namespace FactoriesGateSystem.Controllers
         [ProducesResponseType(500)]
         public async Task<IActionResult> GetSuppliers([FromQuery] string? name)
         {
-            try
+            if (name == null)
             {
-                if (name == null)
-                {
-                    var supplierdto = await _supplierRepo.GetSupplierAsync();
-                    return Ok(supplierdto);
-                }
-                var filtered = await _supplierRepo.GetSupplierAsync(s => s.Name.Contains(name));
-                return Ok(filtered);
+                var supplier = await _supplierService.GetAllSuppliers();
+                return Ok(supplier);
             }
-            catch (Exception) { return StatusCode(500, "Internal server error."); }
+            var filtered = await _supplierService.GetAllSuppliersWithNameAsync(name);
+            return Ok(filtered);
         }
 
         [HttpGet("{id:int}")]
@@ -40,26 +39,13 @@ namespace FactoriesGateSystem.Controllers
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> GetSupplierByIdAsync(int id)
+        public async Task<IActionResult> GetSupplierById(int id)
         {
             if (id <= 0)
                 return BadRequest("Invalid supplier id.");
-            try
-            {
-                var supplier = await _supplierRepo.GetSupplierByIdAsync(id);
-                if (supplier == null)
-                    return NotFound($"Supplier with id {id} not found.");
 
-                var supplierDto = new SupplierDTO()
-                {
-                    Id = id,
-                    Name = supplier.Name,
-                    Address = supplier.Address,
-                    Phone = supplier.Phone,
-                };
-                return Ok(supplierDto);
-            }
-            catch (Exception) { return StatusCode(500, "Internal server error."); }
+            var supplier = await _supplierService.GetSupplierByIdAsync(id);
+            return Ok(supplier);
         }
 
         [HttpGet("{name:alpha}")]
@@ -68,29 +54,20 @@ namespace FactoriesGateSystem.Controllers
         [ProducesResponseType(500)]
         public async Task<IActionResult> GetSuppliersByName(string name)
         {
-            try
-            {
-                var supplier = await _supplierRepo.GetSupplierAsync(s => s.Name.Contains(name));
-                return Ok(supplier);
-            }
-            catch (Exception) { return StatusCode(500, "Internal server error."); }
+            var supplier = await _supplierService.GetSuppliersByNameAsync(name);
+            return Ok(supplier);
         }
 
         [HttpPost]
         [ProducesResponseType(typeof(SupplierDTO), 200)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> AddSupplier([FromBody] SupplierDTO supplierDto)
+        public async Task<IActionResult> AddSupplier([FromBody] CreateSupplierDTO dto)
         {
-            try
-            {
-                var factoryId = Request.Cookies["FactoryId"];
-                if (factoryId == null)
-                    return Unauthorized();
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
 
-                var supplier = await _supplierRepo.AddSupplierAsync(supplierDto, int.Parse(factoryId));
-                return Ok(supplier);
-            }
-            catch (Exception) { return StatusCode(500, "Internal server error."); }
+            var supplier = await _supplierService.AddSupplierAsync(dto);
+            return Ok(supplier);
         }
 
         [HttpPut("{id}")]
@@ -98,22 +75,16 @@ namespace FactoriesGateSystem.Controllers
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(500)]
-        public async Task<IActionResult> UpdateSupplier(int id, [FromBody] UpdateSupplierDTO supplierDto)
+        public async Task<IActionResult> UpdateSupplier(int id, [FromBody] UpdateSupplierDTO dto)
         {
             if (id <= 0)
                 return BadRequest("Invalid supplier id.");
 
-            if(supplierDto.Name == null && supplierDto.Address == null && supplierDto.Phone == null)
+            if(dto.Name == null && dto.Address == null && dto.Phone == null)
                 return BadRequest("At least one field (Name or Address or Phone) must be provided.");
 
-            try
-            {
-                var supplier = await _supplierRepo.UpdateSupplierAsync(id, supplierDto);
-                if (supplier == null) { return NotFound($"No Supplier with id: {id}."); }
-                return Ok(supplier);
-            }
-            catch (Exception)
-            { return StatusCode(500, "Internal server error"); }
+            var supplier = await _supplierService.UpdateSupplierAsync(id, dto);
+            return Ok(supplier);
         }
 
         [HttpDelete("{id}")]
@@ -125,19 +96,9 @@ namespace FactoriesGateSystem.Controllers
         {
             if (id <= 0)
                 return BadRequest("Invalid supplier id.");
-            try
-            {
-                var supplier = await _supplierRepo.DeleteSupplierAsync(id);
-                if (supplier == null) { return NotFound($"No Supplier with id: {id}."); }
 
-                var supplierDto = new DeleteSupplierDTO()
-                {
-                    Id = id,
-                    Name = supplier.Name,
-                };
-                return Ok(supplierDto);
-            }
-            catch (Exception) { return StatusCode(500, "Internal server error"); }
+            var supplier = await _supplierService.DeleteSupplierAsync(id);
+            return Ok(supplier);
         }
     }
 } 
