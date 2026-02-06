041d3dad3802814d4ae686f2c58256d85480beb2
Ameer Mialeh <ameerinad91@gmail.com>
2026-02-06 18:11:23 +0200

    Add Middleware to handle Exceptions

---
+++ Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace FactoriesGateSystem.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                context.Response.StatusCode = ex.StatusCode;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(new { error = ex.Message })
+                );
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(
+                    JsonSerializer.Serialize(new { error = "Internal Server Error" })
+                );
+            }
+        }
+    }
+}
